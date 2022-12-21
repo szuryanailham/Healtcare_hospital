40a31cd29f258380fa8becfbe70304c14b9d9099
Imam Ramadhan <imamrmdn99@gmail.com>
2022-12-21 21:27:41 +0700

    starter project

---
+++ app/src/main/java/com/example/selfhealth_app/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.selfhealth_app
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //menghilangkan ActionBar
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_splash)
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }, 3000L) //3000 L = 3 detik
+    }
+}
