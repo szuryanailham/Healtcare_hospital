35dfe69f2371b42156e2afe3d9b2ab399d34b9bf
mfadlurr14 <fadhlurrohmanhakim01@gmail.com>
2022-12-25 19:58:01 +0700

    update : activity_room.xml end RoomActivity.kt but not finished

--- app/src/main/java/com/example/selfhealth_app/RoomActivity.kt
+++ app/src/main/java/com/example/selfhealth_app/RoomActivity.kt
@@ -2,11 +2,34 @@ package com.example.selfhealth_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
 import com.example.selfhealth_app.R
 
 class RoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
+    }
+    fun onRadioButtonClicked(view: ) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.rdb1 ->
+                    if (checked) {
+
+                    }
+                R.id.rdb2 ->
+                    if (checked) {
+
+                    }
+                R.id.rdb3 ->
+                    if (checked) {
+
+                    }
+            }
+        }
     }
 }
