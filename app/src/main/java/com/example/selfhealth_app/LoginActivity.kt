8bbb10496ba91b63bed2b76a205bcc4b684274b4
Imam Ramadhan <imamrmdn99@gmail.com>
2022-12-26 12:55:11 +0700

    update: sharedpref for login

---
+++ app/src/main/java/com/example/selfhealth_app/LoginActivity.kt
@@ -0,0 +1,51 @@
+package com.example.selfhealth_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.selfhealth_app.helper.Constant
+import com.example.selfhealth_app.helper.PreferencesHelper
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var  sharedpref : PreferencesHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        sharedpref = PreferencesHelper(this)
+
+        buttonLogin.setOnClickListener {
+            if (editEmail.text.isNotEmpty() && editPassword.text.isNotEmpty()) {
+                saveSession(editEmail.text.toString(), editPassword.text.toString())
+                showMessage("Login Success")
+                moveIntent()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (sharedpref.getBoolean(Constant.PREF_IS_LOGIN)) {
+            moveIntent()
+        }
+    }
+
+    private fun moveIntent() {
+        startActivity(Intent(this, UserActivity::class.java))
+        finish()
+    }
+
+    private fun saveSession(email: String, password: String) {
+        sharedpref.put(Constant.PREF_EMAIL, email)
+        sharedpref.put(Constant.PREF_PASSWORD, password)
+        sharedpref.put(Constant.PREF_IS_LOGIN, true)
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+}
